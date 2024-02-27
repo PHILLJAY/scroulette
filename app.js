eb517e452e11545103a06422ee2b08db8fd59b2d
Philip Jasionowski <philip.jasionowski@gmail.com>
2024-02-27 15:15:50 -0500

    formated to look pretty :D

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.static(path.join(__dirname + "/public")));
 
 app.use((req, res) => {
-    res.status(404);
-    res.send("<h1>Error 404 resource not found</h1>")
-})
+  res.status(404);
+  res.send("<h1>Error 404 resource not found</h1>");
+});
 
 app.listen(3000, () => {
-    console.log("App Listening on port 3000")
-})
+  console.log("App Listening on port 3000");
+});
